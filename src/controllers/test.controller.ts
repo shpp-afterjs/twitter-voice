2d66217e943cc8c95e772b9f3347b613981af0db
VERHOVNA-RADA <62828748+VERHOVNA-RADA@users.noreply.github.com>
2021-12-19 15:09:27 +0200

    [fix] fixed unknown types

--- src/controllers/test.controller.ts
+++ src/controllers/test.controller.ts
@@ -1,3 +1,5 @@
-export function Test(req: any, res: any) {
+import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
+
+export function Test(req: FastifyRequest, res: FastifyReply) {
     res.send('Hello World!')
 }
