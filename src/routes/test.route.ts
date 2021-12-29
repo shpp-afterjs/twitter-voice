d938e66b20f54c781dc7305edb3e9250b4861ae0
shavlenkov <pashashavlenkov@gmail.com>
2021-12-29 22:54:11 +0200

    [fix] added any type

--- src/routes/test.route.ts
+++ src/routes/test.route.ts
@@ -1,6 +1,6 @@
 import {Test} from "../controllers/test.controller";
 
-export async function routes (fastify, options) {
+export async function routes (fastify: any, options: any) {
     fastify.get('/test', Test)
 
     //there can be more routes
