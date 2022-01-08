0de27994621345c3e57d710194658996385488f4
shavlenkov <pashashavlenkov@gmail.com>
2022-01-08 22:18:18 +0200

    [feat] fill in the table of users

--- src/controllers/test.controller.ts
+++ src/controllers/test.controller.ts
@@ -7,6 +7,15 @@ import {getRepository} from "typeorm";
 export function Test(req: FastifyRequest, res: FastifyReply) {
     const random = Math.floor(Math.random() * 1000000000) //this is for test
     return getRepository(Users).save({
-        name: String(random)
+        name: String(random),
+        lastname: String(random),
+        gender: "male",
+        birthday: "2022-01-04",
+        email: String(random),
+        password: String(random),
+        ip: String(random),
+        dateLastAdd: "2022-01-04",
+        dateLastUpdate: "2022-01-04",
+        dateCreateAcount: "2022-01-04"
     });
 }
