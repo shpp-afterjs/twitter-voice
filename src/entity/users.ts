493369102e6954b16e251c6597f2b49b4ae50b01
shavlenkov <pashashavlenkov@gmail.com>
2022-01-08 22:11:08 +0200

    [feat] added user entity

--- src/entity/users.ts
+++ src/entity/users.ts
@@ -3,8 +3,37 @@ import {Entity, PrimaryGeneratedColumn, Column, BaseEntity, PrimaryColumn} from 
 @Entity('users')
 export class Users extends BaseEntity {
     @PrimaryGeneratedColumn()
-    id: number
+    id: number;
 
-    @Column('text',{nullable:true})
+    @Column()
     name: string;
+
+    @Column()
+    lastname: string;
+
+    @Column()
+    gender: "male" | "female";
+
+    @Column()
+    birthday: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    ip: string;
+
+    @Column()
+    dateLastAdd: string;
+
+    @Column()
+    dateLastUpdate: string;
+
+    @Column()
+    dateCreateAccount: string;
+
+
 }
