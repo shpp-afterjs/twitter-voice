a32ad2cfd331f0692cdd83e9344d79eba4dd4369
Mac <mac@MacBook-Air-Mac.local>
2022-01-13 16:28:21 +0200

    [feat] added crypto and example for validator

--- src/controllers/test.controller.ts
+++ src/controllers/test.controller.ts
@@ -1,21 +1,43 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 import {Users} from "../entity/users";
 import {getRepository} from "typeorm";
+import crypto from 'crypto'
 
+import { requestValidator } from "../validators/request";
+import { validate } from "class-validator";
 
-
-export function Test(req: FastifyRequest, res: FastifyReply) {
+export async function Test(req: FastifyRequest, res: FastifyReply) {
     const random = Math.floor(Math.random() * 1000000000) //this is for test
-    return getRepository(Users).save({
+
+    const salt = crypto.randomBytes(16).toString('hex')
+    let request = new requestValidator()
+    request.text = (req.body as requestValidator).text
+    const validation = await validate(request)
+    if(validation.length > 0) {
+        return {
+            ok: 'false',
+            errors: validation
+        }
+    }
+
+    const passwordHash = crypto.pbkdf2Sync((req.body as requestValidator).text, salt, 1000, 60, 'sha512').toString('hex')
+
+    const user = await getRepository(Users).save({
         name: String(random),
         lastname: String(random),
         gender: "male",
         birthday: "2022-01-04",
         email: String(random),
-        password: String(random),
+        password: passwordHash,
         ip: String(random),
         dateLastAdd: "2022-01-04",
         dateLastUpdate: "2022-01-04",
-        dateCreateAcount: "2022-01-04"
+        dateCreateAccount: "2022-01-04"
     });
+
+    return {
+        name: user.name,
+        lastname: user.lastname,
+        // тут можно вернуть ещё больше параметров
+    }
 }
