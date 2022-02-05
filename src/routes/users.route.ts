a5005779b9a537c8af0652e03f6b7a6ee262f6ad
Павел Шавленков <69006300+shavlenkov@users.noreply.github.com>
2022-02-05 17:15:23 +0200

    [fix] updated routes

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,11 +1,11 @@
-import {createUser, getUsers, getUser, deleteUser, updateUser} from "../controllers/users.controller";
+import * as UserControllers from "../controllers/users.controller";
 import {FastifyInstance, FastifyServerOptions} from "fastify";
 
 export async function routes (fastify: FastifyInstance, options: FastifyServerOptions) {
-    fastify.post('/create', createUser)
-    fastify.get('/', getUsers)
-    fastify.get('/:userId', getUser)
-    fastify.delete("/:userId", deleteUser)
-    fastify.put("/update/:userId", updateUser)
+    fastify.post('/', UserControllers.create)
+    fastify.get('/', UserControllers.findAll)
+    fastify.get('/:userId', UserControllers.findOne)
+    fastify.delete("/:userId", UserControllers.deleteOne)
+    fastify.put("/:userId", UserControllers.updateOne)
     //there can be more routes
 }
